24bfe50febe28a20006b75cdf8b37e2d8d6e6fb5
Yonatan Cohen <photoycm@gmail.com>
2023-11-25 23:07:01 +0200

    section 2 drop anim

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const navMenu = document.querySelector('#nav-menu');
-const sectionsContainer = document.querySelectorAll('#sections');
-const sections = document.querySelectorAll('.section');
+const sectionsContainer = document.querySelector('#sections');
+const sectionWraps = gsap.utils.toArray(".section-wrap");
+const sections = gsap.utils.toArray(".section-wrap .section");
+const slider = document.querySelector('#slides');
+const slides = gsap.utils.toArray("#slides .slide-card");
+const sliderSection = document.querySelector('#section-07');
 
 // set logo charecter color-white class
 gsap.set("#logo_svg .color-white", {
@@ -13,11 +17,22 @@ gsap.set("#logo_svg .color-green", {
 gsap.set("#nav-menu", {
     backgroundImage: "linear-gradient(270deg, rgba(239, 239, 239, 0.34) 0%, rgba(228, 228, 228, 0.34) 100%)"
 });
+gsap.set("#drop", {
+    // opacity: 0
+});
 
-// set all "#section-06 .img-22" to start off screen to the right
-// gsap.set("#section-06 .img-22", {
-//      x: "200%"
-// });
+let sliderTween = gsap.to(slides, {
+    xPercent: -100 * (slides.length - 1),
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#section-07",
+        pin: true,
+        scrub: 1,
+        snap: 1 / (slides.length - 1),
+        // base vertical scrolling on how wide the container is so it feels more natural.
+        end: () => "+=" + slider.offsetWidth
+    }
+});
 
 const headerTL = gsap.timeline({
     scrollTrigger: {
@@ -37,11 +52,28 @@ headerTL
 .to('#nav-menu', {
     backgroundImage: "linear-gradient(270deg, rgba(149, 149, 149, 0.34) 0%, rgba(128, 128, 128, 0.54) 40%)"
 }, '<');
+let section2MidHeight = document.querySelector('#section-02').offsetHeight / 2;
+let drop = gsap.to('#drop', {
+    scrollTrigger: {
+        id: 'drop',
+        trigger: '#section-02',
+        start: () => 'top 90%',
+        end: () => (section2MidHeight + 165) + ' center',
+        toggleActions: 'play reverse play stop',
+        scrub: 1,
+        markers: true,
+        // pin: true,
+        // pinSpacing: false
+    },
+    x: -285,
+    y: section2MidHeight + 165,
+    rotate: 0,
+    duration: 3
+});
 
 const section6TL = gsap.timeline({
     scrollTrigger: {
         trigger: '#section-06',
-        markers: true,
         start: '300px 100%',
         end: '80% bottom',
         toggleActions: 'play reverse play reverse',
